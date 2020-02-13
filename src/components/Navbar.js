c6d5925c299f3858459c9c40c9c91dcf7a6d22cf
Greg <g.rutherford10@gmail.com>
2020-02-13 15:19:03 +0000

    complete basic navbar with styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
-const Navbar = () => <div>Navbar</div>;
+const Navbar = () => (
+  <nav className="nav">
+    <ul className="menu-nav">
+      <li className="menu-nav_item">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="menu-nav_item">
+        <Link to="/projects">Projects</Link>
+      </li>
+      <li className="menu-nav_item">
+        <Link to="/contact">Contact Me</Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
