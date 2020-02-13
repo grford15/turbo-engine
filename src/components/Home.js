10d8c2aaff685faa8c9539ff0747534d3282fb74
Greg <g.rutherford10@gmail.com>
2020-02-13 16:04:33 +0000

    basic home page completed with styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import './Home.css';
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+  <section className="home">
+    <h2>Hi, My name is </h2>
+    <h1 className="home-name">
+      Greg <span className="home-name_last">Rutherford</span>
+    </h1>
+    <h2>Software Developer</h2>
+  </section>
+);
 
 export default Home;
